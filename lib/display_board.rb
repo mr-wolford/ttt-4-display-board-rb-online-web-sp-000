66db13be5486168d9f207c6f8aa732478ff66f81
Matt Wolford <matthew.r.wolford@gmail.com>
2020-11-17 03:18:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 def display_board(game)
   puts " #{game[0] }| #{game[1]} | #{game[2]} "
-  puts "___________"
+  puts "-----------"
   puts " #{game[3] }| #{game[4]} | #{game[5]} "
-  puts "___________"
+  puts "-----------"
   puts " #{game[6] }| #{game[7]} | #{game[8]} "
 end
 
