3a61f9777873329ecbf27a5fa6ba2a6a20729308
Matt Wolford <matthew.r.wolford@gmail.com>
2020-11-17 03:05:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
-board = [" "," "," "," "," "," "," "," "," "]
-display_board(board)
+def display_board
+  puts "   | X |   "
+  puts "___________"
+  puts "   | X |   "
+  puts "___________"
+  puts "   | X |   "
+end
+
+display_board
